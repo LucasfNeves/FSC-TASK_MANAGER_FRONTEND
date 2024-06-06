583a126572fba65e939f5e20741c8836c09b349c
Lucas Farias <lucasfarias.ln@gmail.com>
2024-06-05 22:11:26 -0300

    feat: add function to delete task

--- src/components/TaskItem/index.jsx
+++ src/components/TaskItem/index.jsx
@@ -1,7 +1,21 @@
 import { Trash } from "phosphor-react";
 import styles from "./styles.module.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const TaskItem = ({ task }) => {
+    const handleDeleteTask = async (id) => {
+        try {
+            await axios.delete(
+                `https://fsc-task-manager-backend-1.onrender.com/tasks/${id}`
+            );
+            toast.success("Tarefa deletada com sucesso");
+        } catch (error) {
+            console.log(error.message);
+            toast.error("Erro ao deletar a tarefa");
+        }
+    };
+
     return (
         <div className={styles.taskItemContainer}>
             <div className={styles.taskDescription}>
@@ -24,7 +38,10 @@ const TaskItem = ({ task }) => {
                 </label>
             </div>
 
-            <div className={styles.delete}>
+            <div
+                className={styles.delete}
+                onClick={() => handleDeleteTask(task._id)}
+            >
                 <Trash size={18} color="#F97474" weight="bold" />
             </div>
         </div>
