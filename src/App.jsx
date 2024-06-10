f710e09a1be4b3e283860ee3ef0e7dd924b72310
Lucas Farias <lucasfarias.ln@gmail.com>
2024-06-10 15:44:31 -0300

    feat: add login router

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import { Sidebar } from "./components/Sidebar";
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 
 const App = () => {
     return (
        <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
        </Routes>
     );
 };
