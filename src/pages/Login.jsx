205f6b14afd37622eb18a7936523530c5ea081b8
Lucas Farias <lucasfarias.ln@gmail.com>
2024-06-10 15:35:25 -0300

    feat: add login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import logo from '../assets/images/1111.png'
+import { CustomButton } from '../components/CustomButton'
+
+import styles from './Login.module.css'
+
+const Login = () => {
+    return (
+        <div className={styles.loginContainer}>
+            <img src={logo} alt="Full Stack Task Manager" />
+            <div className={styles.buttonContainer}>
+                <CustomButton>
+                    Entrar
+                </CustomButton>
+            </div>
+        </div>
+    )
+}
+
+export {Login}
